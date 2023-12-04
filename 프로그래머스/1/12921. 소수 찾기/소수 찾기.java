a922c183b1f4b30ce74bfa9d3374bd76b1ee35eb
wonjinKim <150232059+ceronar@users.noreply.github.com>
2023-12-04 09:48:05 +0900

    [level 1] Title: 소수 찾기, Time: 30.73 ms, Memory: 52.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        boolean[] isPrime = new boolean[n+1];
+        Arrays.fill(isPrime,true);
+        isPrime[0] = isPrime[1] = false;
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if(isPrime[i]){
+                for(int j = i*i; j <= n; j += i){
+                    isPrime[j] = false;
+                }
+            }
+        }
+        for(int i = 0; i < isPrime.length; i ++) {
+            if(isPrime[i] == true) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
