8e601f57b831d22e1cfe5fd15b588437aab61566
wonjinKim <150232059+ceronar@users.noreply.github.com>
2025-07-28 17:54:36 +0900

    [level 1] Title: 약수의 합, Time: 0.05 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -3,11 +3,10 @@ class Solution {
         if (n <= 0) {
             return 0;
         }
-        int sum = 0;
         if (n == 1) {
             return 1;
         }
-        sum = 1;
+        int sum = 1;
         int limit = (int) Math.sqrt(n);
         for (int i = 2; i <= limit; i++) {
             if (n % i == 0) {
